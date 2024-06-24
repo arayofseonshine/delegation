8385011d2a50ffe3d654e7581e2deb5f200688c8
arayofseonshine <arayofseonshine@naver.com>
2024-06-24 09:16:38 +0900

    delegation

--- client/lib/dom/bindEvent.js
+++ client/lib/dom/bindEvent.js
@@ -4,13 +4,13 @@
 
 function bindEvent(node,type,handler){
   
-  if(isString(node)) node = getNode(node);
-
-  node.addEventListener(type,handler);
-
-  return () => node.removeEventListener(type,handler);
-
-}
-
-
-
+	if(isString(node)) node = getNode(node);
+  
+	node.addEventListener(type,handler);
+  
+	return () => node.removeEventListener(type,handler);
+  
+  }
+  
+  
+  
